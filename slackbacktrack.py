4a19cb2cb5551e0773bf3cd6938705fb75c63505
Luke Davia <lukealization@gmail.com>
2016-12-11 11:58:43 +1300

    probably better to store messages in the program rather than in a file for now

--- slackbacktrack.py
+++ slackbacktrack.py
@@ -8,19 +8,28 @@ slack = SlackClient(api_token)
 # create a connection with Slack.
 slack.rtm_connect()
 
-# message counter
-mesage_counter = 0
+# create a list of messages.
+messages = []
+
 # loop inside this forever.
 while True:
     # read the events that slack has sent us.
     events = slack.rtm_read()
+
     for event in events:
+
+        # Wanna make sure the event type is correct, we should probably also
+        # record message edits and deletes too.
         if event['type']  == "message":
-        # for each event increment our message counter.
 
-        # if the message counter is below 10,000, add it to a file.
+        # if the number of messages is below 10,000 store it,
+        # if the number of messages is above 10,000, archive the file.
+            if len(messages) < 10:
+                # store
 
-    # if the message counter is above 10,000, archive the file.
+            else:
+                # write to file and post to archives channel
+
 
     # wait one second.
     time.sleep(1)
