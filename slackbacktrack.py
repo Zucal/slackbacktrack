d1cb79c9f3f10c22ecafef57996898d58eff64e4
Zucal <zucalsocial@gmail.com>
2016-12-10 14:51:08 -0800

    Commit 3 - Working on events and the loop structure
    Filling in some of the scaffold in slackbacktrack.py.

--- slackbacktrack.py
+++ slackbacktrack.py
@@ -1,4 +1,5 @@
 from slackclient import SlackClient
+import time
 
 # setup slack API token and create a client.
 api_token = "foo"
@@ -8,15 +9,18 @@ slack = SlackClient(api_token)
 slack.rtm_connect()
 
 # message counter
-
+mesage_counter = 0
 # loop inside this forever.
 while True:
     # read the events that slack has sent us.
+    events = slack.rtm_read()
+    for event in events:
+        if event['type']  == "message":
+        # for each event increment our message counter.
 
-    # for each event increment our message counter.
-
-    # if the message counter is below 10,000, add it to a file.
+        # if the message counter is below 10,000, add it to a file.
 
     # if the message counter is above 10,000, archive the file.
 
     # wait one second.
+    time.sleep(1)
