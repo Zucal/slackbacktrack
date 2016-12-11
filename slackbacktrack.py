23eeaa1630c1299fcb373b18e0ed2f34613dc66e
Luke Davia <lukealization@gmail.com>
2016-12-11 23:27:27 +1300

    add error resilience

--- slackbacktrack.py
+++ slackbacktrack.py
@@ -30,12 +30,22 @@ def main():
     Sets up the application.
     """
 
+    # setup logging
+    logging.basicConfig(filename='slackbacktrack.log')
+
     # setup slack API token and create a client.
     api_token = sys.argv[1]
     slack = SlackClient(api_token)
 
     # create a connection with Slack.
-    slack.rtm_connect()
+    has_connected = False
+    while not has_connected:
+        connection_outcome = slack.rtm_connect()
+
+        if connection_outcome:
+            has_connected = True
+        else:
+            logging.error("{} Fatal error: {}".format(time.strftime("%Y-%m-%d %H:%M:%S"), "Could not connect"))
 
     # create a list of messages.
     messages = []
@@ -44,7 +54,6 @@ def main():
     while True:
         # read the events that slack has sent us.
         events = slack.rtm_read()
-        print('tick')
 
         for event in events:
             print(event)
@@ -56,15 +65,17 @@ def main():
                 # if the number of messages is below 10,000 store it,
                 # if the number of messages is above 10,000, archive the file.
                 if len(messages) < 2:
-                    print("appending...")
                     # append the event to our messages list.
                     messages.append(event)
 
                 else:
-                    print("archiving...")
                     # write to file and post to archives channel
                     archive(messages, '#general', slack)
                     messages.clear()
+
+            # We should attempt to reconnect if we get this message
+            if event['type'] == "goodbye":
+                pass
 
         # wait one second.
         time.sleep(1)
